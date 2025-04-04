1468af3d2a2a699dca338bc59db5675f72d93ce1
Luis Alberto Torres Díaz <lalberto.torresd@hotmail.com>
2025-04-04 17:17:10 +0200

    Actualiza la generación de combinaciones en main.py para incluir combinaciones de 1 a 3 letras del hebreo, invirtiendo el orden para reflejar la escritura de derecha a izquierda.

--- main.py
+++ main.py
@@ -29,17 +29,22 @@ hebreo = (
 # Definimos un diccionario para las letras finales
 letras_finales = {"כ": "ך", "מ": "ם", "נ": "ן", "פ": "ף", "צ": "ץ"}
 
-# Generamos todas las combinaciones de dos letras del hebreo
-combinaciones = list(itertools.product(hebreo, repeat=2))
-
-# Convertimos las combinaciones de tuplas a strings, considerando las letras finales
-combinaciones_str = []
-for combinacion in combinaciones:
-    letra1, letra2 = combinacion
-    if letra2 in letras_finales:
-        combinaciones_str.append(letra1 + letras_finales[letra2])
-    else:
-        combinaciones_str.append(letra1 + letra2)
+# Generamos todas las combinaciones de 1, 2 y 3 letras del hebreo
+combinaciones = []
+for length in range(1, 5):  # Longitudes de 1 a 3
+    for combinacion in itertools.product(hebreo, repeat=length):
+        if length == 1:
+            combinaciones.append(
+                "".join(combinacion)[::-1]
+            )  # Invertir para derecha a izquierda
+        else:
+            letra1, *resto = combinacion
+            if resto and resto[-1] in letras_finales:
+                combinaciones.append(
+                    (letra1 + letras_finales[resto[-1]])[::-1]
+                )  # Invertir
+            else:
+                combinaciones.append("".join(combinacion)[::-1])  # Invertir
 
 # Imprimimos las combinaciones
-print(combinaciones_str)
+print(combinaciones)
